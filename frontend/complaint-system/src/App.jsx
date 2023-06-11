b5b7eae2f7112f68807e3496d346a7220b7f4531
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-06-11 22:59:05 +0400

    Added Navbar and RecipeReviewCard component added to root component

--- frontend/complaint-system/src/App.jsx
+++ frontend/complaint-system/src/App.jsx
@@ -1,33 +1,38 @@
+
+//!React
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
+//!MateriUI React
+import { Container,Grid } from '@material-ui/core'
+
+
+//!Custom Components
+import Navbar from '../components/ComplaintsNavbar.jsx'
+import RecipeReviewCard from '../components/ComplaintsCard.jsx'
+
+
+import useStyles from '../style/style.js'
+
+
+//*App
 function App() {
-  const [count, setCount] = useState(0)
+
+  const classes = useStyles()
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar/>
+      <br/><br/>
+      <Container className={classes.cardGrid} maxWidth="md">
+        <Grid container spacing={4}>          
+              <RecipeReviewCard />
+              <RecipeReviewCard />
+              <RecipeReviewCard />
+        </Grid>
+      </Container>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
