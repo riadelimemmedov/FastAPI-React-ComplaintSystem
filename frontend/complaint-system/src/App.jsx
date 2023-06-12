23194eca6273c6fcfae445e91adf11030b1320e3
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-06-13 00:44:44 +0400

    Integrate react pagination

--- frontend/complaint-system/src/App.jsx
+++ frontend/complaint-system/src/App.jsx
@@ -12,13 +12,39 @@ import Navbar from '../components/ComplaintsNavbar.jsx'
 import RecipeReviewCard from '../components/ComplaintsCard.jsx'
 
 
+//!Third Party Package
+import { ToastContainer, toast } from 'react-toastify';
+import ReactPaginate from 'react-paginate';
+
+
+//!Custom styles
 import useStyles from '../style/style.js'
+import '../style/Pagination.css'
+
 
 
 //*App
 function App() {
 
   const classes = useStyles()
+
+
+  const [complaints,getComplaints] = useState(['item1', 'item2', 'item3','item4','item5','item6','item7'])
+  const [currentPage, setCurrentPage] = useState(0);
+
+
+  const itemsPerPage = 3; // Number of items per page
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const paginatedItems = complaints.slice(startIndex, endIndex); // Array of items for the current page
+
+
+  //*handlePageChange
+  const handlePageChange = ({ selected }) => {
+      setCurrentPage(selected);
+  };
+  
 
   return (
     <div className="App">
@@ -27,8 +53,34 @@ function App() {
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>          
               <RecipeReviewCard />
-              <RecipeReviewCard />
-              <RecipeReviewCard />
+
+
+              {
+                paginatedItems?.length > 0 ? 
+                (
+                  <>
+                    {paginatedItems.map((complaint,index)=>(
+                      <p key={index}>{complaint}</p>
+
+                    ))}
+
+                    <ReactPaginate
+                    pageCount={endIndex < complaints.length ? complaints.length/3 : ''} // Total number of pages
+                    pageRangeDisplayed={3} // Number of pages to display in the pagination
+                    marginPagesDisplayed={2} // Number of pages to display before and after the current page
+                    onPageChange={handlePageChange} // Callback function to handle page changes
+                    containerClassName={'pagination'} // CSS class for the pagination container
+                    activeClassName={'active'} 
+                    disabled={true}
+          />
+                  </>
+                )
+                :
+                (
+                  <p>Not found</p>
+                )
+              }
+
         </Grid>
       </Container>
 
