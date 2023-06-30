ffc0215bb4b82715a2ee610d8421d024ca6b6806
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-06-30 15:37:57 +0400

    Registered all url component

--- frontend/complaint-system/src/main.jsx
+++ frontend/complaint-system/src/main.jsx
@@ -1,8 +1,25 @@
+
+//!React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//!React Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//!Custom Components
+import SignIn from '../routes/SignIn.jsx'
+import SignUp from '../routes/SignUp'
+
+
+//?Render component to client site dynamically
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<App/>}/>
+            <Route path="/login" element={<SignIn/>}/>
+            <Route path="/register" element={<SignUp/>}/>
+        </Routes>
+    </BrowserRouter>
 )
